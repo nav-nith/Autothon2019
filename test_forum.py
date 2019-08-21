382e73aabe863f00e927f2c92e2f764e2c759b12
pradeep2010 <pradeep.venkatesh@gmail.com>
2019-08-21 10:50:05 +0530

    support threading

--- test_forum.py
+++ test_forum.py
@@ -1,6 +1,8 @@
 from pom.forum import Forum
 from pom.video_response import VideoReponse
 import logging
+import threading
+import time
 
 
 log = logging.getLogger("TEST")
@@ -8,7 +10,21 @@ log.setLevel(logging.DEBUG)
 
 
 class TestForum:
-    def test_one(self):
+    def test_base(self, mobile_driver, chrome_driver):
+        mobile_thread = threading.Thread(thread=run, args=(mobile_driver,))
+        browser_thread = threading.Thread(thread=run, args=(chrome_driver,))
+
+        log.debug(f"Starting mobile test...")
+        mobile_thread.start()
+        log.debug(f"Starting chrome test...")
+        browser_thread.start()
+
+        log.debug("Joining mobile thread...")
+        mobile_thread.join()
+        log.debug("Joining browser thread...")
+        browser_thread.join()
+
+    def run(self, driver):
         f = Forum()
         video_response = VideoReponse()
 
@@ -21,7 +37,7 @@ class TestForum:
         f.search_for(video_title)
         f.scroll_to_view(video_title)
 
-        # driver.get_screenshot_as_file("screenshot.png")
+        driver.get_screenshot_as_file(f"screenshot_{time.strftime('%Y%m%d-%H%M%S')}.png")
 
         f.play_video()
 
